a2ee3493573c4489c6694c1dca63001ea52db035
jamber751 <jaamber751@gmail.com>
2023-11-06 20:03:28 +0600

    Node class

---
+++ Graph/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace graph_traversal.Graph
+{
+	public class Node
+	{
+        public int id { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+        public int? dayVisited { get; set; } = null;
+        public int? visitedFrom { get; set; } = null;
+    }
+}
+
